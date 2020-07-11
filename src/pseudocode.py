3a236b5357bf1d9cb85c78346e3fa018f89fd233
Vince Choi <vscseries@gmail.com>
2020-07-11 14:37:37 -0700

    Create pseudocode.py
    Adding Src folder with psudeocode

---
+++ src/pseudocode.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Acronym Expansion using Synonymous Word Integration on BERT
+# Authors: Vince Sing Choi and Piyush Puranik
+
+
+
+# Train synonymous model (SM) on corpus to learn word asscoiations
+# Requirements:
+# 1. Corpus of synonyms
