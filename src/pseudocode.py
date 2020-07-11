c11c0678ec356f7b94d891e9125eec1ebe83c3e9
Vince Choi <vscseries@gmail.com>
2020-07-11 15:03:16 -0700

    Update pseudocode.py

--- src/pseudocode.py
+++ src/pseudocode.py
@@ -4,6 +4,35 @@
 
 
 
-# Train synonymous model (SM) on corpus to learn word asscoiations
 # Requirements:
-# 1. Corpus of synonyms
+    # 1. Corpus/Dictionary of synonyms: https://github.com/zaibacu/thesaurus
+    # 2. BERT model
+    # 3. Find/create corpus to mask and test
+
+
+# Let D be the document in question, A be the detected acronym, AE be the expansion of A
+
+# Alg 1: Preparing the data
+# For A in D : 
+#    mask(AE) 
+# D' <- D with masked AE 
+# return D' 
+
+
+# Let C be the collection of candidate words for the expansion
+# Alg 2: Find candidate expansions
+# For i in |A|:
+    # C_i <-  BERT(D')
+    # C_i <- C_i + SYN(C_i) // Append synonyms of words found by BERT
+    # max_score <- 0
+    # For j in C_i :
+        # score = Acronym_score(C_j)
+        # if max_score < score
+        # C' <- C_j
+# return C' 
+
+
+# Alg 3: Acronym_score
+
+
+
