4f9b2172133d0466e09b957a9898ae21af4cdc4f
Vince Choi <vscseries@gmail.com>
2020-07-13 13:24:14 -0700

    Update synonym_test.py

--- src/synonym_test.py
+++ src/synonym_test.py
@@ -6,6 +6,7 @@
 # en_thesarus.jsonl contains json line file with 169001 entries of words structured in: word, synonym, pos, key
 
 import jsonlines
+from nltk.corpus import wordnet as wn
 
 
 counter = 0
@@ -13,6 +14,14 @@ counter = 0
 with jsonlines.open("../Thesaurus/en_thesaurus.jsonl", mode = "r") as reader: 
     for obj in reader:
         if (obj["word"] == "abandon"):
+            print(obj)
             print(obj["synonyms"]);
         #counter += 1
 #print(counter)
+
+
+
+# additional synonyms nltk
+
+abandon = wn.synset("abandon.n.01")
+print(abandon.hypernyms())
