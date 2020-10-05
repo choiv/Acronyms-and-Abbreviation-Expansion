e45cfd9ef198bdf1df2f76f1efdb91efb748cda3
preppie22 <piyush.puranik@unlv.edu>
2020-10-04 21:24:46 -0700

    reapply ugly hack to parse_csv.py

--- dataset/parse_csv.py
+++ dataset/parse_csv.py
@@ -8,12 +8,13 @@ datapath = "E:\\PythonProjects\\acronym-dataset\\"
 answer_dir = "E:\\PythonProjects\\acronym-dataset\\answer_output\\"
 question_dir = "E:\\PythonProjects\\acronym-dataset\\question_output\\" 
 
-# hack to close file in loop without doing comparison check
 # answers = open("C:\\Users\\Admin\\Desktop\\virtual\\requirements.txt")
 # questions = open("C:\\Users\\Admin\\Desktop\\virtual\\requirements.txt")
 
-
+# hack to close file in loop without doing comparison check
 dataset = open(datapath + "dataset_acr_exp.csv")
+answers = open(datapath + "dataset_acr_exp.csv") 
+questions = open(datapath + "dataset_acr_exp.csv")
 reader = csv.reader(dataset)
 
 current_file = ""
@@ -21,9 +22,9 @@ file_name = ""
 for row in reader:
     file_name = str(row[0])
     if (file_name != current_file): 
-        if (not answers.closed()):
+        if (not answers.closed):
             answers.close()
-        if (not questions.closed()):
+        if (not questions.closed):
             questions.close()
         current_file = file_name
         answer_file = answer_dir + file_name + "_answer.txt"
