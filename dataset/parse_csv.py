0f43e8bb8a6535d936fb37c5bf7aabe9910fee8d
preppie22 <piyush.puranik@unlv.edu>
2020-10-04 21:16:36 -0700

    fix ugly file close hack.

--- dataset/parse_csv.py
+++ dataset/parse_csv.py
@@ -4,13 +4,13 @@
 import csv
 
 # paths
-datapath = "C:\\Users\\Admin\\Desktop\\virtual\\"
-answer_dir = "C:\\Users\Admin\\Desktop\\virtual\\answer_output\\"
-question_dir = "C:\\Users\Admin\\Desktop\\virtual\\question_output\\" 
+datapath = "E:\\PythonProjects\\acronym-dataset\\"
+answer_dir = "E:\\PythonProjects\\acronym-dataset\\answer_output\\"
+question_dir = "E:\\PythonProjects\\acronym-dataset\\question_output\\" 
 
 # hack to close file in loop without doing comparison check
-answers = open("C:\\Users\\Admin\\Desktop\\virtual\\requirements.txt")
-questions = open("C:\\Users\\Admin\\Desktop\\virtual\\requirements.txt")
+# answers = open("C:\\Users\\Admin\\Desktop\\virtual\\requirements.txt")
+# questions = open("C:\\Users\\Admin\\Desktop\\virtual\\requirements.txt")
 
 
 dataset = open(datapath + "dataset_acr_exp.csv")
@@ -21,8 +21,10 @@ file_name = ""
 for row in reader:
     file_name = str(row[0])
     if (file_name != current_file): 
-        answers.close()
-        questions.close()
+        if (not answers.closed()):
+            answers.close()
+        if (not questions.closed()):
+            questions.close()
         current_file = file_name
         answer_file = answer_dir + file_name + "_answer.txt"
         question_file = question_dir + file_name + "_question.txt"
